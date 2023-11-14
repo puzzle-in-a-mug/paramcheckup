44b767fb4c0bcd58e0ffd73f2d7e1113dc52260b
Anderson Marcos Dias Canteli <andersonmdcanteli@gmail.com>
2023-11-14 18:38:13 -0300

    Update test_is_float_or_int.py
    Improved tests

--- tests/numbers/test_is_float_or_int.py
+++ tests/numbers/test_is_float_or_int.py
@@ -2,11 +2,7 @@
 
 --------------------------------------------------------------------------------
 Command to run at the prompt:
-
     python -m unittest -v tests/numbers/test_is_float_or_int.py
-    or
-    python -m unittest -b tests/numbers/test_is_float_or_int.py
-
 --------------------------------------------------------------------------------
 """
 
@@ -20,72 +16,200 @@ import numpy as np
 from paramcheckup.numbers import is_float_or_int
 
 
-os.system('cls')
-
+os.system("cls")
 
 
 class Test_is_float_or_int(unittest.TestCase):
-
-
     @classmethod
     def setUpClass(cls):
-        pass
+        cls.number = 12
+        cls.param_name = "alpha"
+        cls.kind = "function"
+        cls.kind_name = "ttest"
+        cls.stacklevel = 3
+        cls.error = True
 
     def test_outputs(self):
-        output = is_float_or_int(1, "parametro", "func_name")
+        output = is_float_or_int(
+            self.number,
+            self.param_name,
+            self.kind,
+            self.kind_name,
+            self.stacklevel,
+            self.error,
+        )
         self.assertTrue(output, msg="not True when must be True")
 
+        output = is_float_or_int(
+            number=self.number,
+            param_name=self.param_name,
+            kind=self.kind,
+            kind_name=self.kind_name,
+            stacklevel=self.stacklevel,
+            error=self.error,
+        )
+        self.assertTrue(output, msg="not True when must be True")
 
+    def test_raises_error_false(self):
+        values = [
+            "\n asda",
+            [1],
+            (1, 1, 1),
+        ]
+        for value in values:
+            with self.assertRaises(SystemExit, msg="Does not raised SystemExit"):
+                output = is_float_or_int(
+                    number=value,
+                    param_name=self.param_name,
+                    kind=self.kind,
+                    kind_name=self.kind_name,
+                    stacklevel=self.stacklevel,
+                    error=False,
+                )
+
+    def test_pass_error_false(self):
+        values = [
+            1,
+            1.1,
+            np.int32(1),
+            np.int64(1),
+            np.uint(1),
+            np.int32(10),
+            np.int64(10),
+            np.uint(1),
+            np.float16(1),
+        ]
+
+        for value in values:
+            result = is_float_or_int(
+                number=value,
+                param_name=self.param_name,
+                kind=self.kind,
+                kind_name=self.kind_name,
+                stacklevel=self.stacklevel,
+                error=False,
+            )
+            self.assertTrue(result, msg=f"An error was raised when number is {value}")
 
     def test_string(self):
-        with self.assertRaises(TypeError, msg="Does not raised error when value is a string"):
-            is_float_or_int("a", param_name="parameter", func_name="func_name")
-        with self.assertRaises(TypeError, msg="Does not raised error when value is a string"):
-            is_float_or_int("hausdasuda \n", param_name="parameter", func_name="func_name")
+        with self.assertRaises(
+            TypeError, msg="Does not raised error when value is a string"
+        ):
+            output = is_float_or_int(
+                number="self.number",
+                param_name=self.param_name,
+                kind=self.kind,
+                kind_name=self.kind_name,
+                stacklevel=self.stacklevel,
+                error=self.error,
+            )
+        with self.assertRaises(
+            TypeError, msg="Does not raised error when value is a string"
+        ):
+            output = is_float_or_int(
+                number="\n asda",
+                param_name=self.param_name,
+                kind=self.kind,
+                kind_name=self.kind_name,
+                stacklevel=self.stacklevel,
+                error=self.error,
+            )
 
     def test_list(self):
-        with self.assertRaises(TypeError, msg="Does not raised error when value is a list"):
-            is_float_or_int([1], param_name="parameter", func_name="func_name")
-        with self.assertRaises(TypeError, msg="Does not raised error when value is a list of list"):
-            is_float_or_int([[1]], param_name="parameter", func_name="func_name")
+        with self.assertRaises(
+            TypeError, msg="Does not raised error when value is a list"
+        ):
+            output = is_float_or_int(
+                number=[1],
+                param_name=self.param_name,
+                kind=self.kind,
+                kind_name=self.kind_name,
+                stacklevel=self.stacklevel,
+                error=self.error,
+            )
+        with self.assertRaises(
+            TypeError, msg="Does not raised error when value is a list of list"
+        ):
+            output = is_float_or_int(
+                number=[[1]],
+                param_name=self.param_name,
+                kind=self.kind,
+                kind_name=self.kind_name,
+                stacklevel=self.stacklevel,
+                error=self.error,
+            )
 
     def test_tuple(self):
-        with self.assertRaises(TypeError, msg="Does not raised error when value is a tuple"):
-            is_float_or_int((1,), param_name="parameter", func_name="func_name")
-        with self.assertRaises(TypeError, msg="Does not raised error when value is a tuple"):
-            is_float_or_int((1,1,1), param_name="parameter", func_name="func_name")
-        with self.assertRaises(TypeError, msg="Does not raised error when value is a tuple"):
-            is_float_or_int((1, (1,), 1), param_name="parameter", func_name="func_name")
+        with self.assertRaises(
+            TypeError, msg="Does not raised error when value is a tuple"
+        ):
+            output = is_float_or_int(
+                number=(1,),
+                param_name=self.param_name,
+                kind=self.kind,
+                kind_name=self.kind_name,
+                stacklevel=self.stacklevel,
+                error=self.error,
+            )
+        with self.assertRaises(
+            TypeError, msg="Does not raised error when value is a tuple"
+        ):
+            output = is_float_or_int(
+                number=(1, 1, 1),
+                param_name=self.param_name,
+                kind=self.kind,
+                kind_name=self.kind_name,
+                stacklevel=self.stacklevel,
+                error=self.error,
+            )
+        with self.assertRaises(
+            TypeError, msg="Does not raised error when value is a tuple"
+        ):
+            output = is_float_or_int(
+                number=(1, (1,), 1),
+                param_name=self.param_name,
+                kind=self.kind,
+                kind_name=self.kind_name,
+                stacklevel=self.stacklevel,
+                error=self.error,
+            )
 
     def test_empty(self):
-        with self.assertRaises(TypeError, msg="Does not raised error when no value was passed"):
-            is_float_or_int(param_name="parameter", func_name="func_name")
+        with self.assertRaises(
+            TypeError, msg="Does not raised error when no value was passed"
+        ):
+            output = is_float_or_int(
+                param_name=self.param_name,
+                kind=self.kind,
+                kind_name=self.kind_name,
+                stacklevel=self.stacklevel,
+                error=self.error,
+            )
 
     def test_pass(self):
-        result = is_float_or_int(1, param_name="parameter", func_name="func_name")
-        self.assertTrue(result, msg = "An error was raised when value was 1")
+        values = [
+            1,
+            1.1,
+            np.int32(1),
+            np.int64(1),
+            np.uint(1),
+            np.int32(10),
+            np.int64(10),
+            np.uint(1),
+            np.float16(1),
+        ]
 
-        result = is_float_or_int(np.int32(1), param_name="parameter", func_name="func_name")
-        self.assertTrue(result, msg = "An error was raised when value was np.int32(1)")
-
-        result = is_float_or_int(np.int64(1), param_name="parameter", func_name="func_name")
-        self.assertTrue(result, msg = "An error was raised when value was np.int64(1)")
-
-        result = is_float_or_int(np.uint(1), param_name="parameter", func_name="func_name")
-        self.assertTrue(result, msg = "An error was raised when value was np.uint(1)")
-
-        result = is_float_or_int(1, param_name="parameter", func_name="func_name")
-        self.assertTrue(result, msg = "An error was raised when value was 1")
-
-        result = is_float_or_int(np.int32(1), param_name="parameter", func_name="func_name")
-        self.assertTrue(result, msg = "An error was raised when value was np.int32(1)")
-
-        result = is_float_or_int(np.int64(1), param_name="parameter", func_name="func_name")
-        self.assertTrue(result, msg = "An error was raised when value was np.int64(1)")
-
-        result = is_float_or_int(np.uint(1), param_name="parameter", func_name="func_name")
-        self.assertTrue(result, msg = "An error was raised when value was np.uint(1)")
+        for value in values:
+            result = is_float_or_int(
+                number=value,
+                param_name=self.param_name,
+                kind=self.kind,
+                kind_name=self.kind_name,
+                stacklevel=self.stacklevel,
+                error=self.error,
+            )
+            self.assertTrue(result, msg=f"An error was raised when number is {value}")
 
 
 if __name__ == "__main__":
-    unittest.main()    
+    unittest.main()
