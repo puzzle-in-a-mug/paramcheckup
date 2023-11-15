867e66549e51c2f25407d3b6dcadf0943240412c
Anderson Marcos Dias Canteli <andersonmdcanteli@gmail.com>
2023-11-15 20:11:15 -0300

    Update test_is_float.py
    improved tests

--- tests/types/test_is_float.py
+++ tests/types/test_is_float.py
@@ -2,11 +2,7 @@
 
 --------------------------------------------------------------------------------
 Command to run at the prompt:
-
     python -m unittest -v tests/types/test_is_float.py
-    or
-    python -m unittest -b tests/types/test_is_float.py
-
 --------------------------------------------------------------------------------
 """
 
@@ -20,66 +16,95 @@ import numpy as np
 from paramcheckup.types import is_float
 
 
-os.system('cls')
-
+os.system("cls")
 
 
 class Test_is_float(unittest.TestCase):
-
-
     @classmethod
     def setUpClass(cls):
-        pass
+        cls.value = 0.05
+        cls.param_name = "alpha"
+        cls.kind = "function"
+        cls.kind_name = "ttest"
+        cls.stacklevel = 3
+        cls.error = True
+        cls.values = [
+            "anderson",
+            1,
+            np.int32(1),
+            np.int64(1),
+            [1],
+            [[1]],
+            (1,),
+            (1, 1, 1),
+            (1, (1,), 1),
+            None,
+            {"a": 1},
+            set([1, 2, 3]),
+        ]
 
     def test_outputs(self):
-        output = is_float(0.1, "parametro", "func_name")
+        output = is_float(
+            self.value,
+            self.param_name,
+            self.kind,
+            self.kind_name,
+            self.stacklevel,
+            self.error,
+        )
         self.assertTrue(output, msg="not True when must be True")
-        output = is_float(value=.1, param_name="parametro", func_name="func_name")
-        self.assertTrue(output, msg="not True when must be True")        
 
-    def test_string(self):
-        with self.assertRaises(TypeError, msg="Does not raised error when value is a string"):
-            is_float("a", param_name="parameter", func_name="func_name")
-        with self.assertRaises(TypeError, msg="Does not raised error when value is a string"):
-            is_float("auisdhsa9d8ysadasd9oasdasdonha nsad\n", param_name="parameter", func_name="func_name")
+        output = is_float(
+            value=self.value,
+            param_name=self.param_name,
+            kind=self.kind,
+            kind_name=self.kind_name,
+            stacklevel=self.stacklevel,
+            error=self.error,
+        )
+        self.assertTrue(output, msg="not True when must be True")
 
-    def test_int(self):
-        with self.assertRaises(TypeError, msg="Does not raised error when value is int"):
-            is_float(1, param_name="parameter", func_name="func_name")
-        with self.assertRaises(TypeError, msg="Does not raised error when value is int"):
-            is_float(np.int32(1), param_name="parameter", func_name="func_name")
-        with self.assertRaises(TypeError, msg="Does not raised error when value is int"):
-            is_float(np.int64(1), param_name="parameter", func_name="func_name")
+    def test_pass_erro_ffalse(self):
+        output = is_float(
+            self.value,
+            self.param_name,
+            self.kind,
+            self.kind_name,
+            self.stacklevel,
+            False,
+        )
+        self.assertTrue(output, msg="not True when must be True")
 
-    def test_list(self):
-        with self.assertRaises(TypeError, msg="Does not raised error when value is a list"):
-            is_float([1], param_name="parameter", func_name="func_name")
-        with self.assertRaises(TypeError, msg="Does not raised error when value is a list"):
-            is_float([[1]], param_name="parameter", func_name="func_name")
+    def test_raises(self):
+        for value in self.values:
+            with self.assertRaises(
+                TypeError,
+                msg=f"Does not raised error when value is a {type(value).__name__}",
+            ):
+                output = is_float(
+                    value=value,
+                    param_name=self.param_name,
+                    kind=self.kind,
+                    kind_name=self.kind_name,
+                    stacklevel=self.stacklevel,
+                    error=self.error,
+                )
 
-    def test_tuple(self):
-        with self.assertRaises(TypeError, msg="Does not raised error when value is a tuple"):
-            is_float((1,), param_name="parameter", func_name="func_name")
-        with self.assertRaises(TypeError, msg="Does not raised error when value is a tuple"):
-            is_float((1,1,1), param_name="parameter", func_name="func_name")
-        with self.assertRaises(TypeError, msg="Does not raised error when value is a tuple"):
-            is_float((1, (1,), 1), param_name="parameter", func_name="func_name")
-
-    def test_empty(self):
-        with self.assertRaises(TypeError, msg="Does not raised error when no value was passed"):
-            is_float(param_name="parameter", func_name="func_name")
-
-    def test_pass(self):
-        result = is_float(1.1, param_name="parameter", func_name="func_name")
-        self.assertTrue(result, msg = "An error was raised when value was 1.1")
-
-        result = is_float(np.float32(1), param_name="parameter", func_name="func_name")
-        self.assertTrue(result, msg = "An error was raised when value was np.float32(1)")
-
-        result = is_float(np.float64(1), param_name="parameter", func_name="func_name")
-        self.assertTrue(result, msg = "An error was raised when value was np.float64(1)")  
-  
+    def test_raises_error_false(self):
+        for value in self.values:
+            with self.assertRaises(
+                SystemExit,
+                msg=f"Does not raised SystemExit when value is a {type(value).__name__}",
+            ):
+                output = is_float(
+                    value=value,
+                    param_name=self.param_name,
+                    kind=self.kind,
+                    kind_name=self.kind_name,
+                    stacklevel=self.stacklevel,
+                    error=False,
+                )
 
 
 if __name__ == "__main__":
-    unittest.main()    
+    unittest.main()
