ceca31c647d62cb3caa545e7a0739487a01b66f8
Anderson Marcos Dias Canteli <andersonmdcanteli@gmail.com>
2023-11-15 23:32:33 -0300

    Update types.py
    refactored is_subplots

--- paramcheckup/types.py
+++ paramcheckup/types.py
@@ -852,50 +852,91 @@ def is_str(value, param_name, kind, kind_name, stacklevel=4, error=True):
     return True
 
 
-def is_subplots(value, param_name, func_name):
-    """This function checks whether a variable *value* is of the *matplotlib.axes.SubplotBase* type.
+@docs.docstring_parameter(
+    value=docs.VALUE["type"],
+    value_desc=docs.VALUE["description"],
+    param_name=docs.PARAM_NAME["type"],
+    param_name_desc=docs.PARAM_NAME["description"],
+    kind=docs.KIND["type"],
+    kind_desc=docs.KIND["description"],
+    kind_name=docs.KIND_NAME["type"],
+    kind_name_desc=docs.KIND_NAME["description"],
+    stacklevel=docs.STACKLEVEL["type"],
+    stacklevel_desc=docs.STACKLEVEL["description"],
+    error=docs.ERROR["type"],
+    error_desc=docs.ERROR["description"],
+)
+def is_subplots(value, param_name, kind, kind_name, stacklevel=4, error=True):
+    """This function checks whether a variable `value` is of the `matplotlib.axes.SubplotBase` type.
 
     Parameters
     ----------
-    value : any type
-        The variable that is tested as being of *matplotlib.axes.SubplotBase* type;
-    param_name : str
-        The name of the parameter that received the variable *value*';
-    func_name : str
-        The name of the function that utilizes the parameter *param_name*;
+    {value}
+        {value_desc} `matplotlib.axes.SubplotBase` ;
+    {param_name}
+        {param_name_desc} `value`;
+    {kind}
+        {kind_desc}
+    {kind_name}
+        {kind_name_desc}
+    {stacklevel}
+        {stacklevel_desc}
+    {error}
+        {error_desc}
+
 
     Returns
     -------
-    True
-        If variable *value* **IS** of the *matplotlib.axes.SubplotBase* type;
-    TypeError
-        If variable *value* is **NOT** of the *matplotlib.axes.SubplotBase* type;
+    output : True
+        If variable `value` **IS** of the `matplotlib.axes.SubplotBase` type;
+    raises : TypeError
+        If variable `value` is **NOT** of the `matplotlib.axes.SubplotBase` type;
+
 
     Examples
     --------
     >>> from paramcheckup import types
     >>> import matplotlib.pyplot as plt
     >>> fig, ax = plt.subplots()
-    >>> print(types.is_subplots(ax, "axes", "ploting_func"))
-    True
+    >>> output = types.is_subplots(
+        value=ax,
+        param_name="axes",
+        kind="function",
+        kind_name="plot",
+        stacklevel=3,
+        error=True,
+    )
     >>> plt.close()
+    >>> print(output)
+    True
 
 
     >>> from paramcheckup import types
     >>> import matplotlib.pyplot as plt
     >>> fig, ax = plt.subplots()
-    >>> types.is_subplots(fig, "axes", "ploting_func")
-    The parameter 'axes' in function 'ploting_func' must be of type *matplotlib.axes.SubplotBase*, but its type is *Figure*.
+    >>> output = types.is_subplots(
+        value=fig,
+        param_name="axes",
+        kind="function",
+        kind_name="plot",
+        stacklevel=3,
+        error=False,
+    )
+    UserWarning at line 4: The parameter `axes` in function `plot` must be of type `matplotlib.axes.SubplotBase`, but its type is `Figure`.
 
     """
     if isinstance(value, SubplotBase) is False:
-        try:
-            raise TypeError("NotSubplotError")
-        except TypeError:
-            print(
-                f"The parameter '{param_name}' in function '{func_name}' must be of type *matplotlib.axes.SubplotBase*, but its type is *{type(value).__name__}*.\n"
-            )
-            raise
+        user_warning(
+            f"The parameter `{param_name}` in {kind} `{kind_name}` must be of type `matplotlib.axes.SubplotBase`, but its type is `{type(value).__name__}`.\n",
+            stacklevel=stacklevel,
+        )
+        if error is False:
+            sys.exit(1)
+        else:
+            try:
+                raise TypeError("NotSubplotError")
+            except TypeError:
+                raise
     return True
 
 
